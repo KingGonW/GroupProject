cc01968cf8b9841b7dc2de61b4d9904545553128
KingGonW <wong.kinghang1997@gmail.com>
2022-06-14 15:57:54 +0100

    Bank Account

---
+++ src/BankAcc.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class BankAcc {
+    public static void main (String[] args){
+        Random rand= new Random();
+        int sort_code = rand.nextInt(999999);
+        System.out.println(sort_code);
+        //return String.format("%06d", sort_code);
+        int accnum = rand.nextInt(99999999);
+        System.out.println(accnum);
+    }
+}
