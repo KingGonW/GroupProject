2107fd3f1da34ec8b0f15d01179c8c0518d148fb
Mohsen <63107432+Mohsenrobin@users.noreply.github.com>
2022-06-15 12:34:55 +0100

    push 2

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,56 @@
 public class Customer {
+
+    private long id;
     private String name;
+    private String lastName;
+    private String emailAddress;
+    private String phoneNumber;
+
+    public Customer(long id, String name, String lastName, String emailAddress, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
