5a214a179e6dea27df3fa927ebaf7a4e0ccbd3ce
Tamara <tamaradepaula99@gmail.com>
2022-06-16 19:00:23 +0100

    created business account class

--- src/BusinessAccount.java
+++ src/BusinessAccount.java
@@ -1,19 +1,2 @@
-public class BusinessAccount extends BankAcc {
-
-
-    final String businessSortCode;
-    private int businessAccountNumber;
-
-    public BusinessAccount(String businessSortCode, int businessAccountNumber) {
-        this.businessSortCode = businessSortCode;
-        this.businessAccountNumber = businessAccountNumber;
-    }
-
-    public String getBusinessSortCode() {
-        return businessSortCode;
-    }
-
-    public int getBusinessAccountNumber() {
-        return businessAccountNumber;
-    }
+public class BusinessAccount {
 }
