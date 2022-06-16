d4eb5cf27f430ca16e315200568a2ab806edcfa4
Tamara <tamaradepaula99@gmail.com>
2022-06-16 19:29:05 +0100

    redid andrews commit

--- src/BusinessAccount.java
+++ src/BusinessAccount.java
@@ -1,2 +1,19 @@
-public class BusinessAccount {
+public class BusinessAccount extends BankAcc {
+
+
+    final String businessSortCode;
+    private int businessAccountNumber;
+
+    public BusinessAccount(String businessSortCode, int businessAccountNumber) {
+        this.businessSortCode = businessSortCode;
+        this.businessAccountNumber = businessAccountNumber;
+    }
+
+    public String getBusinessSortCode() {
+        return businessSortCode;
+    }
+
+    public int getBusinessAccountNumber() {
+        return businessAccountNumber;
+    }
 }
