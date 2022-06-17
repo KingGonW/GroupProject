06c3ae97b73ff2f0c511aa86cc4c7d0a3a867797
Tamara <tamaradepaula99@gmail.com>
2022-06-17 16:02:04 +0100

    made accountType, sortcode and interest rate final. changed withdrawal if statement

--- src/ISAAccount.java
+++ src/ISAAccount.java
@@ -1,38 +1,28 @@
 public class ISAAccount extends BankAcc{
 
-    private static String accountType = "ISA Account";
-    final String ISAsortCode;
-    private double interestRate;
+    private final String accountType = "ISA Account";
+    private final String ISAsortCode = "22-66-44";
+    private final double interestRate = 0.02;
     private int ISAAccountNumber;
-    private double interest;
-    private double balance;
+    private double yearlyInterest;
 
-    ISAAccount (double balance, int accNum, double interestRate, double initialDeposit,
-                String sortCode, int ISAAccountNumber) {
+
+    public ISAAccount(double balance, int accNum) {
         super(balance, accNum);
-        this.ISAsortCode = "22-66-44";
         this.ISAAccountNumber = accNum;
-        this.setBalance(initialDeposit);
+        /*this.setBalance(initialDeposit);
         if(initialDeposit >= 100) {
             this.setInterest(0.02);
         }
         else if(initialDeposit < 100) {
             System.out.println("must deposit 100 or more to create an ISA account");
-        }
+        }*/
     }
 
     public double getInterest() {
-        return interest;
+        return yearlyInterest;
     }
 
-    public void setInterest(double interest) {
-        this.interest = interest;
-    }
-
-    @Override
-    public void setBalance(double savingsBalance) {
-        balance = savingsBalance;
-    }
 
     public String getSortCode() {
 
@@ -40,33 +30,36 @@ public class ISAAccount extends BankAcc{
     }
 
 
-    public void withdraw(double withdrawAmount) {  //override withdraw method from BankAcc class
-        if(super.getBalance() < 100) {
-            System.out.println("Your account balance is too low, " +
+    public void withdraw(double withdrawAmount) {
+        //override withdraw method from BankAcc class
+        if(super.getBalance() - withdrawAmount < 100) {
+            System.out.println("Your account balance will be too low, " +
                     "to keep your ISA account, please keep balance over 100");
             System.out.println("Would you like to deposit money?");
         }
         else {
             super.withdrawMoney(withdrawAmount);
             System.out.println("You have withdrawn " + withdrawAmount + " from your account" + "\n" +
-                    "Your Balance is now: " + this.balance);
+                    "Your Balance is now: " + this.getBalance());
         }
     }
 
     public void deposit(double depositAmount) {  //override deposit method from BankAcc class
         super.depositMoney(depositAmount);
         System.out.println("You have deposited " + depositAmount +  " from your account." + "\n" +
-                "Balance is now: " + this.balance);
+                "Balance is now: " + this.getBalance());
     }
+
+
 
     public void calInterest() // method to calculate interest on customers most recent balance
     {
-        interestRate = 0.02;
-        double yearlyInterest = interestRate*(365/365);
-        yearlyInterest = balance * yearlyInterest;
-        this.balance += yearlyInterest;
-        System.out.print("Interest accumalated on your balance is: " +
-                yearlyInterest + '\n' + "Your balance is now: " + this.balance);
+
+        yearlyInterest = interestRate*(365/365);
+        yearlyInterest = this.getBalance() * yearlyInterest;
+        this.setBalance(yearlyInterest);
+        System.out.print("Interest accumulated on your balance is: " +
+                yearlyInterest + '\n' + "Your balance is now: " + this.getBalance());
 
 
     }
