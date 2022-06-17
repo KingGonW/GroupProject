4b13d76d2263bbdae0836d8f6c6476fe7aa67ecb
KingGonW <wong.kinghang1997@gmail.com>
2022-06-17 14:04:51 +0100

    Bank Account

--- src/BusinessAccount.java
+++ src/BusinessAccount.java
@@ -1,13 +1,17 @@
 public class BusinessAccount extends BankAcc {
 
-
+    private static String accountType = "Business Account";
     final String businessSortCode;
     private int businessAccountNumber;
 
-    public BusinessAccount(double balance, int accNum, String businessSortCode, int businessAccountNumber) {
+    public BusinessAccount(double balance,double initialDeposit, int accNum, String businessSortCode, int businessAccountNumber) {
         super(balance, accNum);
-        this.businessSortCode = businessSortCode;
-        this.businessAccountNumber = businessAccountNumber;
+        this.businessSortCode = "12-23-24";
+        this.businessAccountNumber = accNum;
+        this.setBalance(initialDeposit);
+        if(initialDeposit <= 100) {
+            System.out.println("must deposit 25 or more to create an Business account");
+        }
     }
 
     public String getBusinessSortCode() {
